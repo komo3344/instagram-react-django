8e9200686af8c16e26a8ab6a6fc41f9c152ac3ea
junyoung <jy940608@naver.com>
2020-07-17 21:37:31 +0900

    Apply useAxios to SuggestionList API

--- frontend/src/components/SuggestionList.js
+++ frontend/src/components/SuggestionList.js
@@ -1,31 +1,25 @@
 import React, {useState, useEffect}from 'react';
-import {Card} from 'antd';
+import {Button, Card} from 'antd';
 import Suggestion from './Suggestion'
-import Axios from 'axios';
+import useAxios from 'axios-hooks';
 import {useAppContext} from 'store';
 import "./SuggestionList.scss";
 
 export default function SuggestionList({style}) {
     const {store: {jwtToken}} = useAppContext()
-    const [userList, setUserList] = useState([])
-    useEffect(() => {
-        async function fetchUserList() {
-            const apiUrl = "http://localhost:8000/accounts/suggestions/"
-            const headers = { Authorization: `JWT ${jwtToken}`}
-            try {
-                const {data} = await Axios.get(apiUrl, {headers})
-                setUserList(data)
-            }
-            catch(error) {
-                console.log(error)
-            }
-        }
-        fetchUserList() 
-    }, [])
+    const headers = { Authorization: `JWT ${jwtToken}`}
+    const [{data: userList, loading, error}, refetch] = useAxios({
+        url: "http://localhost:8000/accounts/suggestions/",
+        headers,
+    })
+    
     return (
         <div style={style}>
+            {loading && <div>loading ...</div>}
+            {error && <div>로딩 중에 에러가 발생했습니다.</div>}
+            <Button onClick={()=> refetch()}>새로고침</Button>
             <Card title="Suggestions for you" size="small">
-                {userList.map(suggestionUser => (
+                {userList && userList.map(suggestionUser => (
                     <Suggestion key={suggestionUser.username} suggestionUser={suggestionUser} />
                 ))}
                 
