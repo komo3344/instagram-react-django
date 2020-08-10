c196561dc61da50957e6da1032ea4cadce662f52
junyoung <jy940608@naver.com>
2020-08-10 20:38:56 +0900

    Add post like, unlike api

--- backend/instagram/serializers.py
+++ backend/instagram/serializers.py
@@ -9,7 +9,16 @@ class AuthorSerializer(serializers.ModelSerializer):
 
 class PostSerializer(serializers.ModelSerializer):
     author = AuthorSerializer(read_only=True) # 여러개라면 many=True 지정
+    is_like = serializers.SerializerMethodField('is_like_field')
+
+    def is_like_field(self, post):
+        # 사용자를 받아와야하는데 self.request.user 은 view 즉 cbv에서 사용가능한데 여기서는 사용이 안된다 => (drf context사용)
+        if 'request' in self.context:
+            user = self.context['request'].user
+            return post.like_user_set.filter(pk=user.pk).exists()
+        return False
+
     class Meta:
         model = Post
-        fields = '__all__'
+        fields = ['id', 'author', 'created_at', 'photo', 'caption', 'location', 'tag_set', 'is_like']
         
--- backend/instagram/views.py
+++ backend/instagram/views.py
@@ -2,8 +2,11 @@ from datetime import timedelta
 from django.db.models import Q
 from django.shortcuts import render
 from django.utils import timezone
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import status
+from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 from .models import Post
 from .serializers import PostSerializer
 
@@ -13,6 +16,12 @@ class PostViewSet(ModelViewSet):
     queryset = Post.objects.all().select_related("author").prefetch_related("tag_set", "like_user_set")
     serializer_class = PostSerializer
     # permission_classes = [AllowAny,] # FIXME: 인증적용
+
+    # drf 에서 임의의 값을 context로 넘겨줄 수 있는데 현재유저를 가져오기 위해 사용
+    def get_serializer_context(self):   
+        context =  super().get_serializer_context()
+        context['request'] = self.request
+        return context
 
     def get_queryset(self):
         # timesince = timezone.now() - timedelta(days=3) 
@@ -32,3 +41,15 @@ class PostViewSet(ModelViewSet):
         # post.save()
         serializer.save(author=self.request.user)
         return super().perform_create(serializer)
+
+    @action(detail=True, methods=["POST"])
+    def like(self, request, pk):
+        post = self.get_object() # ModelViewSet에서 구현이 되어 있기때문에 굳이 get_object_404 할 필요 X
+        post.like_user_set.add(self.request.user)
+        return Response(status.HTTP_201_CREATED)
+
+    @like.mapping.delete
+    def unlike(self, request, pk):
+        post = self.get_object()
+        post.like_user_set.remove(self.request.user)
+        return Response(status.HTTP_204_NO_CONTENT)
