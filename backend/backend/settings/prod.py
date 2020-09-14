ba25bb2d02cf02d62457742e3c188e84b0c45508
junyoung <jy940608@naver.com>
2020-09-15 02:27:05 +0900

    deploy with Azure

---
+++ backend/backend/settings/prod.py
@@ -0,0 +1,43 @@
+from .common import *
+
+# DEBUG = False
+DEBUG = os.environ.get("DEBUG") in ['1', 't', 'true', 'T', 'True']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+
+STATICFILES_STORAGE = "backend.storages.StaticAzureStorage"
+DEFAULT_FILE_STORAGE = "backend.storages.MediaAzureStorage"
+
+# AZURE_ACCOUNT_NAME = os.environ.get("AZURE_ACCOUNT_NAME") # 없으면 None으로 세팅
+
+# 해당 환경변수가 없으면 키에러 발생
+AZURE_ACCOUNT_NAME = os.environ["AZURE_ACCOUNT_NAME"] 
+AZURE_ACCOUNT_KEY = os.environ["AZURE_ACCOUNT_KEY"]
+
+CORS_ORIGIN_WHITELIST = os.environ.get("CORS_ORIGIN_WHITELIST ", "").split(",")
+
+DATABASES = {
+    'default': {
+        "ENGINE": os.environ.get("DB_ENGINE", "django.db.backends.postgresql"),
+        "HOST": os.environ["DB_HOST"],
+        "USER": os.environ["DB_USER"],
+        "PASSWORD": os.environ["DB_PASSWORD"],
+        "NAME": os.environ.get("DB_NAME", "postgres"),
+    },
+}
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "level": "ERROR",
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"], 
+            "level": "ERROR",
+        },
+    },
+}
